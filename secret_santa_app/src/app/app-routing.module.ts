eb1bc11e54171cb4ad1f34a8b862b675aac81429
Beloting <72801389+medialin@users.noreply.github.com>
2022-10-11 14:54:01 +0200

    add draw component

--- secret_santa_app/src/app/app-routing.module.ts
+++ secret_santa_app/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DrawComponent } from './draw/draw.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'draw', component: DrawComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
